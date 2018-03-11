355b06b513119e3f96355d42f67e4550845e2957
Boris Borisov <djslinger77@gmail.com>
2018-03-11 12:32:48 +0300

    Code formating fixed

--- BitFinexAuthApi.php
+++ BitFinexAuthApi.php
@@ -21,13 +21,13 @@ use GuzzleHttp\Client;
  */
 class BitFinexAuthApi extends Controller
 {
-    public function placeorder($volume, $direction)
+    public function Placeorder($volume, $direction)
     {
 
-        $bit_fnx = new BitFnx(); // Created new instance of class
+        $bitFnx = new BitFnx(); // Created new instance of class
 
         /*
-        * Where exactly the request is sent. REST AUTHENTICATED ENDPOINTS
+        * Where exactly the request is sent. REST AUTHENTICATED EndpointS
         * It can be:
         * summary
         * account_fees
@@ -40,23 +40,23 @@ class BitFinexAuthApi extends Controller
         // Create new instance of guzzle and pass $data array as the set of headers
         // 3 values are sent: X-BFX-APIKEY, X-BFX-PAYLOAD, X-BFX-SIGNATURE
         // Function requestPrepare() call and passing $restAuthEndpoint to it as a parameter
-        $z = $bit_fnx->requestPrepare($restAuthEndpoint, $volume, $direction);
+        $z = $bitFnx->requestPrepare($restAuthEndpoint, $volume, $direction);
 
-        //print_r ($z); // Trace output
-        //echo "Payload: " . $bit_fnx->pay . "<br>";
-        //echo "Signature: " . $bit_fnx->sig . "<br>";
+        dump($z); // Dump $z 
+        //echo "Payload: " . $bitFnx->pay . "<br>";
+        //echo "Signature: " . $bitFnx->sig . "<br>";
         //echo "<br>Place order. Volume: " . $volume;
 
-        $api_connection = new Client([
+        $apiConnection = new Client([
             'base_uri' => 'https://api.bitfinex.com/v1/',
             'timeout' => 5 // If make this value small - fatal error occurs
         ]);
 
-        $response = $api_connection->request('POST', $restAuthEndpoint, [
+        $response = $apiConnection->request('POST', $restAuthEndpoint, [
             'headers' => [
-                'X-BFX-APIKEY' => $bit_fnx->api_key,
-                'X-BFX-PAYLOAD' => $bit_fnx->pay,
-                'X-BFX-SIGNATURE' => $bit_fnx->sig
+                'X-BFX-APIKEY' => $bitFnx->apiKey,
+                'X-BFX-PAYLOAD' => $bitFnx->pay,
+                'X-BFX-SIGNATURE' => $bitFnx->sig
             ]
         ]);
 
@@ -67,97 +67,3 @@ class BitFinexAuthApi extends Controller
 
 }
 
-class BitFnx
-{
-    public $pay;
-    public $sig;
-    const API_URL = 'https://api.bitfinex.com';
-
-    public $api_key; // "YcGqIAMGDgTVESLBDTTGQ32Q9DTsL0u.....";
-    private $api_secret; // "H4G2JdRGvsJ0JOKb1GcnDvoC27oVJvN5OU4hz.....";
-    private $api_version = "v1";
-    
-    public function requestPrepare($summary, $volume, $direction) {
-
-        // Assign key values
-        $this->api_key = $_ENV['BIT_FINEX_PUBLIC_API_KEY']; // Api keys go here
-        $this->api_secret = $_ENV['BIT_FINEX_PRIVATE_API_KEY']; // Add them to .env file or in "***"
-
-        // This method call can take two parameters
-        // No params taken. Only first value is sent
-        $request = $this->endpoint($summary);
-
-        $data = array(
-            'request' => $request, // Request params MUST go here. New order: https://bitfinex.readme.io/v1/reference#rest-auth-new-order
-            'symbol'=> 'ETHUSD', // ETHUSD ETCUSD
-            'amount' => $volume, // 0.02
-            'price' => '1000',
-            'exchange' => 'bitfinex',
-            'side' => $direction,
-            // exchange market - exchanger order. market - margin order. If you need to open a short position - use 'market'. It is impossible to go short with 'exchange order'
-            // Funds must be located at Margin wallet if you go short and long.
-            'type' => 'market'
-        );
-
-        return $this->send_auth_request($data);
-    }
-
-    /**
-     * End point and api version
-     * @param $method
-     * @param null $params
-     * @return string
-     */
-    private function endpoint($method, $params = NULL) {
-        $parameters = '';
-
-        if ($params !== NULL) {
-            $parameters = '/';
-            if (is_array($params)) {
-                $parameters .= implode('/', $params);
-            } else {
-                $parameters .= $params;
-            }
-        }
-
-        return "/{$this->api_version}/$method$parameters";
-    }
-
-    /**
-     * Add data to header for authentication purpose
-     * @param $data
-     * @return array
-     */
-    private function prepare_header($data)
-    {
-        $data['nonce'] = (string) number_format(round(microtime(true) * 1000000), 0, '.', '');
-
-        $payload = base64_encode(json_encode($data));
-        $signature = hash_hmac('sha384', $payload, $this->api_secret);
-
-        $this->pay = $payload;
-        $this->sig = $signature;
-
-        return array(
-            'X-BFX-APIKEY: ' . $this->api_key,
-            'X-BFX-PAYLOAD: ' . $payload,
-            'X-BFX-SIGNATURE: ' . $signature
-        );
-    }
-
-    /**
-     * Send a signed HTTP request
-     * @param $data
-     * @return array
-     */
-    private function send_auth_request($data)
-    {
-        $headers = $this->prepare_header($data);
-        return $headers;
-    }
-
-}
-
-function num_to_string($num) {
-    return number_format($num, 2, '.', '');
-}
---
+++ BitFnx.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: slinger
+ * Date: 3/11/2018
+ * Time: 12:29 PM
+ */
+
+namespace App\Http\Controllers\BitFinexAuthApi;
+
+
+class BitFnx
+{
+    public $pay;
+    public $sig;
+    const API_URL = 'https://api.bitfinex.com';
+
+    public $apiKey; // "YcGqIAMGDgTVESLBDTTGQ32Q9DTsL0u.....";
+    private $apiSecret; // "H4G2JdRGvsJ0JOKb1GcnDvoC27oVJvN5OU4hz.....";
+    private $apiVersion = "v1";
+
+    public function requestPrepare($summary, $volume, $direction) {
+
+        // Assign key values
+        $this->apiKey = $_ENV['BIT_FINEX_PUBLIC_API_KEY']; // Api keys go here
+        $this->apiSecret = $_ENV['BIT_FINEX_PRIVATE_API_KEY']; // Add them to .env file or in "***"
+
+        // This method call can take two parameters
+        // No params taken. Only first value is sent
+        $request = $this->Endpoint($summary);
+
+        $data = array(
+            'request' => $request, // Request params MUST go here. New order: https://bitfinex.readme.io/v1/reference#rest-auth-new-order
+            'symbol'=> 'ETHUSD', // ETHUSD ETCUSD
+            'amount' => $volume, // 0.02
+            'price' => '1000',
+            'exchange' => 'bitfinex',
+            'side' => $direction,
+            // exchange market - exchanger order. market - margin order. If you need to open a short position - use 'market'. It is impossible to go short with 'exchange order'
+            // Funds must be located at Margin wallet if you go short and long.
+            'type' => 'market'
+        );
+
+        return $this->SendAuthRequest($data);
+    }
+
+    /**
+     * End point and api version
+     * @param $method
+     * @param null $params
+     * @return string
+     */
+    private function Endpoint($method, $params = NULL) {
+        $parameters = '';
+
+        if ($params !== NULL) {
+            $parameters = '/';
+            if (is_array($params)) {
+                $parameters .= implode('/', $params);
+            } else {
+                $parameters .= $params;
+            }
+        }
+
+        return "/{$this->apiVersion}/$method$parameters";
+    }
+
+    /**
+     * Add data to header for authentication purpose
+     * @param $data
+     * @return array
+     */
+    private function prepareHeader($data)
+    {
+        $data['nonce'] = (string) number_format(round(microtime(true) * 1000000), 0, '.', '');
+
+        $payload = base64_encode(json_encode($data));
+        $signature = hash_hmac('sha384', $payload, $this->apiSecret);
+
+        $this->pay = $payload;
+        $this->sig = $signature;
+
+        return array(
+            'X-BFX-APIKEY: ' . $this->apiKey,
+            'X-BFX-PAYLOAD: ' . $payload,
+            'X-BFX-SIGNATURE: ' . $signature
+        );
+    }
+
+    /**
+     * Send a signed HTTP request
+     * @param $data
+     * @return array
+     */
+    private function SendAuthRequest($data)
+    {
+        $headers = $this->prepareHeader($data);
+        return $headers;
+    }
+
+}
